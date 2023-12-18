c2e274a8b61dde6027e4e3cc33b40598cc1baf9a
Isabella Basso <isabellabdoamaral@usp.br>
2023-12-18 00:17:11 -0300

    lets just recurse dis bad boi

--- 23/day12.py
+++ 23/day12.py
@@ -1,112 +1,89 @@
 from dataclasses import dataclass
 from enum import Enum
 from pprint import pprint
+import re
 
 
 class Token(Enum):
     unknown = "?"
     damaged = "#"
+    operational = "."
 
 
-@dataclass
-class Record:
-    groups: list[list[Token]]
-    checksums: list[int]
-
-    @classmethod
-    def from_raw(cls, raw: str) -> "Record":
-        raw_record, raw_sizes = raw.split()
-        groups = [
-            [Token(c) for c in group]
-            for group in raw_record.strip(".").split(".")
-            if group
-        ]
-        sizes = list(map(int, raw_sizes.split(",")))
-        return Record(groups, sizes)
+def next_state(record: str) -> int:
+    return next((i for i, t in enumerate(record) if t != record[0]), 0)
 
 
-def count(checksums: list[int]) -> int:
-    return sum(checksums) + len(checksums) - 1 if checksums else 0
-
-
-def count_arr(group: list[Token], size: int, remaining: list[int]) -> int:
-    rem_count = count(remaining)
-
-    if len(group) < size + rem_count:
-        return 0
-
+def count_arr(record: str, sizes: list[int]) -> int:
     # breakpoint()
-    if Token.damaged in group:
-        next_dmg = group.index(Token.damaged)
-
-        if not remaining:
-            # sequence has to be at known damage
-            return 1
-
-        if size < next_dmg:
-            # sequence can go up to the first known damage
-            arr = len(group[:next_dmg]) - size
-            print("meow", arr)
-        else:
-            # sequence has to be close to the first known damage
-            arr = 1
-            print("auau", arr)
-
-        if remaining:
-            arr *= count_arr(group[next_dmg + size + 1 :], remaining[0], remaining[1:])
-            print("meow 2", arr)
-
-    elif remaining:
-        arr = sum(
-            [
-                count_arr(group[i + 1 :], remaining[0], remaining[1:])
-                for i in range(size, len(group) - rem_count + 1)
-            ]
-        )
-    else:
-        # sequence can be any position
-        arr = len(group) - size + 1
-
-    return arr
-
-
-def count_arrangements(record: Record, gi: int = 0, start: int = 0) -> int:
-    if gi == len(record.groups):
-        if start != len(record.checksums):
+    print(record, sizes)
+    if not sizes:
+        if Token.damaged.value in record:
             return 0
         return 1
+    if not record:
+        return 0
 
-    cks = record.checksums
-    group = record.groups[gi]
-    end = len(cks)
-    # try to fit the most amount of sequences on the current group
-    # the record is only valid if there's at least one way to fit every sequence
-    arrangements = 0
-    while end > start:
-        if count(cks[start:end]) <= len(group):
-            if cnt := count_arr(group, cks[start], cks[start + 1 : end]):
-                if branch := count_arrangements(record, gi + 1, end):
-                    arrangements += cnt * branch
-                    print(arrangements, "with ck", cks[start], ":", count(cks[start + 1 : end]), "on", ''.join([g.value for g in group]))
-                else:
-                    print("discarded", cks[start], ":", count(cks[start + 1 : end]), "on", ''.join([g.value for g in group]))
-        end -= 1
+    rem = sum([size + 1 for size in sizes]) - 1
 
-    return arrangements
+    match Token(record[0]):
+        case Token.unknown:
+            # fork
+            # usable_size = sum([len(group) for group in record.split(".")])
+            # if usable_size -
+            #     return 0
+            # else:
+            #     return count_arr(
+            #         Token.damaged.value + record[1:], sizes
+            #     ) + count_arr(Token.operational.value + record[1:], sizes)
+            #
+            if rem > len(record):
+                print("giving up on")
+                return 0
 
+            if rem != len(record):
+                print("forking useless")
+                useless_fork = count_arr(record[1:], sizes)
+            else:
+                useless_fork = 0
+            print("forking")
+            return count_arr(Token.damaged.value + record[1:], sizes) + useless_fork
+        case Token.damaged:
+            # make sure that the next damaged is within the current size
+            if sizes[0] > len(record):
+                print("giving up on")
+                return 0
 
-def flatten(xss):
-    return [x for xs in xss for x in xs]
+            for i in range(sizes[0]):
+                if record[i] == Token.operational.value:
+                    print("giving up on")
+                    return 0
+
+            if sizes[0] < len(record) and record[sizes[0]] == Token.damaged.value:
+                print("cant violate contiguous group")
+                return 0
+
+            print("using", sizes[0])
+            return count_arr(record[sizes[0] + 1 :], sizes[1:])
+        case Token.operational:
+            # fuck 'em
+            if len(record) > 1:
+                print("skipping operational")
+                return count_arr(record[1:], sizes)
+
+            print("giving up on", record)
+            return 0
 
 
 arrangements = 0
 for line in open("day12.in").readlines():
-    print(line)
-    record = Record.from_raw(line)
-    # breakpoint()
-    cnt = count_arrangements(record)
-    print("Count:", cnt)
+    record, raw_sizes = line.split()
+    record = re.sub(r"\.+", ".", record).strip(".")
+    sizes = list(map(int, raw_sizes.split(",")))
+    cnt = count_arr(record, sizes)
     arrangements += cnt
+    print(cnt)
+    print()
 
 
 print(arrangements)
