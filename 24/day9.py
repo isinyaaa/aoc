4b288200f228f4a1588bd22919a03197a242902c
Isabella Basso <isabellabdoamaral@usp.br>
2024-12-12 15:17:37 -0300

    enter the linked-list

--- 24/day9.py
+++ 24/day9.py
@@ -1,6 +1,7 @@
+from dataclasses import dataclass
 from pathlib import Path
 
-raw = [int(c) for c in Path("day9.example.in").read_text().strip()]
+raw = [int(c) for c in Path("day9.in").read_text().strip()]
 
 disk = [
     (i + 1) // 2 if i % 2 != 0 else 0 for i, c in enumerate(raw[1:]) for _ in range(c)
@@ -21,52 +22,65 @@ for i in range(size - 1, 1, -1):
 
 print(sum([(i + raw[0]) * e for i, e in enumerate(disk)]))
 
-blocks, gaps = raw[2::2], raw[1::2]
-disk = [
-    (i + 1) // 2 if i % 2 != 0 else 0 for i, c in enumerate(raw[1:]) for _ in range(c)
-]
+
+@dataclass
+class Block:
+    start: int
+    size: int
+    val: int = 0
+    next: "Block | None" = None
+
+
+blocks = Block(0, raw[0], -1)
+lg = blocks
+p = raw[0]
+for i, d in enumerate(raw[1:]):
+    if i % 2 == 0:  # gap
+        lg.next = Block(p, d)
+        lg = lg.next
+    else:
+        lg.next = Block(p, d, (i + 1) // 2)
+        lg = lg.next
+    p += d
 
 
 def defrag(block, size):
-    ld = 0
-    g = 0
-    while gaps[g] == 0:
-        g += 1
-    while g < gaps[g] and ld < len(disk):
-        if disk[ld] == 0 and gaps[g] >= size:
-            gaps[g] -= size
-            while size > 0:
-                disk[ld] = block
-                ld += 1
-                size -= 1
-            while ld < len(disk) and disk[ld] != block:
-                ld += 1
-            while ld < len(disk) and disk[ld] == block:
-                disk[ld] = 0
-                ld += 1
-            break
-        if gaps[g] == 0:
-            while gaps[g] == 0:
-                g += 1
+    g = blocks
+    while g is not None:
+        if g.val == block:
+            return None
+        if g.val == 0:
+            if g.size == size:
+                g.val = block
+                return g.next
+            elif g.size > size:
+                g.val = block
+                g.next = Block(g.start + size, g.size - size, 0, g.next)
+                g.size = size
+                return g.next
+        g = g.next
+    return None
+
+
+bc = len(raw) // 2
+for i, b in enumerate(reversed(raw[2::2])):
+    bid = bc - i
+    if b := defrag(bid, b):
+        while b is not None:
+            if b.val == bid:
+                b.val = 0
+                break
+            b = b.next
         else:
-            g += 1
-        while ld < len(disk) and disk[ld] == 0:
-            ld += 1
+            raise ValueError("block not found")
 
 
-for i in range(len(blocks)):
-    defrag(len(blocks) - i, blocks[len(blocks) - i - 1])
+checksum = 0
+b = blocks
+while b is not None:
+    if b.val > 0:
+        for x in range(b.size):
+            checksum += (b.start + x) * b.val
+    b = b.next
 
-
-# ld = 0
-# for i in range(size - 1, 1, -1):
-#     if disk[i] == 0:
-#         continue
-#
-#     while ld < size and ld < i:
-#         if disk[ld] == 0:
-#             disk[ld], disk[i] = disk[i], disk[ld]
-#             break
-#         ld += 1
-
-print(sum([(i + int(raw[0])) * e for i, e in enumerate(disk)]))
+print(checksum)
